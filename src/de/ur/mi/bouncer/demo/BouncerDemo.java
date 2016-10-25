01ccd644844a4decb17ce5a7463ae25b871d279e
Lyniat <derlichking@gmx.net>
2016-10-25 17:10:33 +0200

    added build

--- src/de/ur/mi/bouncer/demo/BouncerDemo.java
+++ src/de/ur/mi/bouncer/demo/BouncerDemo.java
@@ -2,9 +2,6 @@ package de.ur.mi.bouncer.demo;
 
 import de.ur.mi.bouncer.apps.BouncerApp;
 
-/**
- * Created by Alexander Bazo on 12/10/2016.
- */
 public class BouncerDemo extends BouncerApp {
     @Override
     public void bounce() {
